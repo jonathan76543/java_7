219fa4b9b5530244597e2b9f0096e06301c0d417
jonathan2121 <james.ciro@yahoo.com.co>
2022-03-15 04:03:04 -0500

    prueba 1

---
+++ src/Prueba_1.java
@@ -0,0 +1,70 @@
+public class Prueba_1{
+
+    public static void main(String[] args) {
+
+        Empleados trabajador1=new Empleados("Paco");
+
+        Empleados trabajador2=new Empleados("Ana");
+
+        Empleados trabajador3=new Empleados("lucas");
+
+        Empleados trabajador4=new Empleados("pepe");
+
+        trabajador1.cambiaSeccion("RRHH");
+
+        trabajador3.cambiaSeccion("operario");
+
+        trabajador4.cambiaSeccion("coordinador");
+
+        System.out.println(trabajador1.devuelveDatos());
+
+        System.out.println(trabajador2.devuelveDatos());
+
+        System.out.println(trabajador3.devuelveDatos());
+
+        System.out.println(trabajador4.devuelveDatos());
+
+
+
+    }
+}
+
+
+
+class Empleados<string>{
+
+    public Empleados(String nom){
+
+        nombre=nom;
+
+        seccion="Administracion";
+
+        Id=IdSiguiente;
+
+        IdSiguiente++;
+    }
+
+   
+    public void cambiaSeccion(String seccion){//setter
+
+       this.seccion=seccion;
+
+    }   
+    public <string> String devuelveDatos(){
+
+        return "El nombre es:"+ nombre +"//la seccion es:"+ seccion +"//y el Id=" +Id;
+ 
+     }   
+
+
+    private final String nombre;
+
+    private String seccion;
+
+    private int Id;
+
+    private static int IdSiguiente=1;
+
+    
+
+}   
